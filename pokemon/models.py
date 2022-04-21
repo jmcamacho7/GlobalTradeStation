f008f11f67edd52763fbfc667fbfa3f01921eb3a
jmcamacho7 <camachosanchezjm@gmail.com>
2022-04-21 19:31:52 +0200

    Comienzo de la pagina

--- pokemon/models.py
+++ pokemon/models.py
@@ -1,8 +1,7 @@
 from django.db import models
-
-# Create your models here.
-
-from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 class Pokemon(models.Model):
     id = models.IntegerField(primary_key=True, max_length=3)
@@ -11,7 +10,29 @@ class Pokemon(models.Model):
     nivel = models.IntegerField(max_length=2)
     habilidad = models.CharField(max_length=30)
     objeto = models.CharField(max_length=20)
+    icono = models.CharField(max_length=500)
+    genero = models.CharField(max_length=1)
+    intercambiable = models.CharField(max_length=1)
 
-    intercambiable = models.BooleanField
+class Usuario(models.Model):
+    usuario = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.CharField(max_length=255, blank=True)
+    web = models.URLField(blank=True)
+
+    # Python 3
+    def __str__(self):
+        return self.usuario.username
+
+
+@receiver(post_save, sender=User)
+def crear_usuario_perfil(sender, instance, created, **kwargs):
+    if created:
+        Usuario.objects.create(usuario=instance)
+
+
+@receiver(post_save, sender=User)
+def guardar_usuario_perfil(sender, instance, **kwargs):
+    instance.perfil.save()
+
 
 
